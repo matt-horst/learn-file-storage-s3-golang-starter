01c74ab984b60477b54f0bea2782e4e8a6827ecb
Matt Horst <mattahorst@gmail.com>
2025-10-22 21:19:03 -0600

    impls uploading video thumbnails handler

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -31,7 +33,45 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse video", err)
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, _, err := r.FormFile("thumbnail")
+	if err !=  nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve video form file", err)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read data from form file", err)
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve video info from db", err)
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You must be video's owner", err)
+	}
+
+	thumbnail := thumbnail {
+		data: data,
+		mediaType: "video/",
+	}
+	
+	videoThumbnails[videoID] = thumbnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
